fb49acb12f7ad6f63bd0d994857881dc08ccb733
zhang <zhang745426@outlook.com>
2018-09-14 19:16:56 +0800

    将authorisation改成了authorization，下次更新的时候服务器也会更新这个地址。请注意

--- app/src/main/java/com/server_auth/AuthServer.java
+++ app/src/main/java/com/server_auth/AuthServer.java
@@ -1,9 +1,9 @@
 package com.server_auth;
 
 public class AuthServer {
-    // 验证接口
-    public static final String AUTH_HOST = "https://codigo.cn/apis/authorisation";
-//    public static String AUTH_HOST = "http://192.168.1.102:8888/authorisation";
+    // 验证接口 过去的验证地址是authorisation
+    public static final String AUTH_HOST = "https://codigo.cn/apis/authorization";
+//    public static String AUTH_HOST = "http://192.168.1.102:8888/authorization";
     public static final String RECORDER_HOST = "https://codigo.cn/apis/py_record";
 //    public static String RECORDER_HOST = "http://192.168.1.102:8888/py_record";
 
